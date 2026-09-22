5b7176d5a06c04a9a5ff477098e9f67edc12d6f2
MOOM09 <mo.tarifi2004@gmail.com>
2026-09-22 23:40:31 +0300

    FIRST TASK IN JS

---
+++ app.js
@@ -0,0 +1,24 @@
+let x =prompt("Whats Your Name");
+let y=prompt("Whats your membershib(Student/Regular)");
+
+if (y=="Student")
+    {
+
+alert("Hello" +" "+ x +" "+ "Scholar");
+    }
+
+    else if(y=="Regular")
+    {
+
+alert("Hello" +" "+ x +" "+ "Member");
+
+    }
+
+    else
+        alert("Hello" +" "+ x +" "+ y);
+
+    let favoriteGenre = prompt("What's your favorite genre (fiction/non-fiction)?");
+    let reserve= prompt("Which book would you like to reserve?");
+    alert("The book he requested is being reserved.");
+   console.log(x + " " + reserve);
+
